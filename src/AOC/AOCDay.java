22a57c15360626909b2e4e73aa21e4c30a40427a
simonefuma <simonefumagalli3@gmail.com>
2023-12-03 14:59:53 +0100

    AOCDay

---
+++ src/AOC/AOCDay.java
@@ -0,0 +1,18 @@
+package AOC;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
+import java.io.File;
+
+public abstract class AOCDay {
+
+    protected static List<String> getInputToList(String year, String day) throws FileNotFoundException {
+        List<String> input = new ArrayList<>();
+        Scanner scanner = new Scanner(new File(System.getProperty("user.dir"), String.format("src/year%s/day%s/input.txt", year, day)));
+        while(scanner.hasNextLine()) input.add(scanner.nextLine());
+        return input;
+    }
+
+}
