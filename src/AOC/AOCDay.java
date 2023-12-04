3fab0e55d90b64168d3a06acdf9f0e6503dc1efb
simonefuma <simonefumagalli3@gmail.com>
2023-12-04 21:32:58 +0100

    add new function for read only one line

--- src/AOC/AOCDay.java
+++ src/AOC/AOCDay.java
@@ -8,11 +8,21 @@ import java.io.File;
 
 public abstract class AOCDay {
 
+    protected static String getInputToString(String year, String day) throws  FileNotFoundException {
+        Scanner scanner = getScanner(year, day);
+        if(scanner.hasNextLine()) return scanner.nextLine();
+        return "";
+    }
+
     protected static List<String> getInputToList(String year, String day) throws FileNotFoundException {
         List<String> input = new ArrayList<>();
-        Scanner scanner = new Scanner(new File(System.getProperty("user.dir"), String.format("src/year%s/day%s/input/input.txt", year, day)));
+        Scanner scanner = getScanner(year, day);
         while(scanner.hasNextLine()) input.add(scanner.nextLine());
         return input;
+    }
+
+    private static Scanner getScanner(String year, String day) throws FileNotFoundException {
+        return new Scanner(new File(System.getProperty("user.dir"), String.format("src/year%s/day%s/input/input.txt", year, day)));
     }
 
 }
